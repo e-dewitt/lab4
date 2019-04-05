2c57a7e64f8ec6a5c82b073fbd9ebd11b0fe78b9
aiftikhar19 <25934283+aiftikhar19@users.noreply.github.com>
2019-04-05 13:42:54 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <stdio.h> 
+  
+#Function Prototypes
+void philosopher();
+void think();
+void get_forks();
+void put_forks();
+void eat();
+
+int main() {
+
+return 0;
+}
+
+void philosopher(){
+  return;
+}
+void think(){
+  return;
+}
+void get_forks(){
+  return;
+}
+void put_forks(){
+  return;
+}
+void eat(){
+  return;
+}
