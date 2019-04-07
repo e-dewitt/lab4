b04790b897199c2dd442722db25ddcb81c4d3803
aiftikhar19 <25934283+aiftikhar19@users.noreply.github.com>
2019-04-07 17:41:11 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 
-#include <pthread.h> 
-#include <semaphore.h> 
-#include <stdio.h> 
+#include<pthread.h> 
+#include<semaphore.h> 
+#include<stdio.h> 
 
-#Global variables
-int state[5];
+//Global variables
+//possible states: thinking, waiting to eat (hungry), eating
+int state[3]; 
 int philosophers[5] = {0, 1, 2, 3, 4};
 
 sem_t mutex;
-sem_t semaphore;;
+sem_t semaphore;
   
-#Function Prototypes
+//Function Prototypes
 void philosopher();
 void think();
 void get_forks();
@@ -18,22 +19,32 @@ void put_forks();
 void eat();
 
 int main() {
-
+  //create pthreads
+  pthread_t pid;
+  
+  //initialize semaphore
+  //sem_init() returns 0 on success; on error, -1 is returned, and errno is set to indicate the error
+  sem_init(&mutex, 0, 1);
+  
 return 0;
 }
 
+//set state of philosopher 
 void philosopher(){
   return;
 }
 void think(){
   return;
 }
+//pick up chopsticks/fork
 void get_forks(){
   return;
 }
+//put down chopsticks/fork 
 void put_forks(){
   return;
 }
+//set state to eating 
 void eat(){
   return;
 }
