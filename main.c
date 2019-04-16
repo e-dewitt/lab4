c39c04845d4f42cd39feb308acb7e0b8f4d04df4
Ellen DeWitt <dewitt.156@osu.edu>
2019-04-15 23:54:31 -0400

    possible solution with mutex locks needs checked!

--- main.c
+++ main.c
@@ -11,10 +11,13 @@
 //possible states: thinking, waiting to eat (hungry), eating
 int state[3]; 
 int philosophers[5] = {0, 1, 2, 3, 4};
-int forks[5] = {0, 1, 2, 3, 4};
+int forks[5] = {1, 1, 1, 1, 1};
 
 int max_time = 10;
 int min_time = 1;
+
+//mutex forx variables
+pthread_mutex_t fork_id[NUM_THREADS];
 
 sem_t mutex;
 sem_t semaphore[5];
@@ -31,8 +34,15 @@ int main() {
 	  //create pthreads
 	  pthread_t thread_id[NUM_THREADS];
 
+
 	  //create count variable i
 	  int i;
+
+	  //initialize mutex threads
+	  for(i = 0; i < NUM_THREADS; i++) {
+	    pthread_mutex_init(&fork_id[i], NULL);
+	  }
+
 	  
 	  //initialize semaphore
 	  //sem_init() returns 0 on success; on error, -1 is returned, and errno is set to indicate the error
@@ -54,8 +64,12 @@ int main() {
 
 	  for(i = 0; i < NUM_THREADS; i++) {
 	    pthread_join(thread_id[i], NULL);
-	}
-  
+	   }
+  	
+  	//destroy the semaphores
+	//sem_destroy(&mutex);
+	//sem_destroy(&semaphores);
+
 	return 0;
 }
 
@@ -78,13 +92,36 @@ void think(int this_philosipher){
 }
 //pick up chopsticks/fork
 void get_forks(int this_philosipher){
-  printf("\nPhilosopher %d is getting forks", this_philosipher);
+  while(pthread_mutex_lock(&fork_id[this_philosipher]) != 0) {
+  	printf("\nPhilosopher %d is waiting for forks", this_philosipher);
+  	sleep(1);
+  }	
+  if(this_philosipher != 4){
+	while(pthread_mutex_lock(&fork_id[this_philosipher + 1]) != 0) {
+	  	printf("\nPhilosopher %d is waiting for forks", this_philosipher);
+	  	sleep(1);
+	}	
+  } else {
+  	while(pthread_mutex_lock(&fork_id[0]) != 0) {
+  		printf("\nPhilosopher %d is waiting for forks", this_philosipher);
+  		sleep(1);
+  	}	
+  }
+  
+
+  printf("\nPhilosopher %d has its forks", this_philosipher);
   
   return;
 }
 //put down chopsticks/fork 
 void put_forks(int this_philosipher){
   printf("\nPhilosopher %d is returning forks", this_philosipher);
+  pthread_mutex_unlock(&fork_id[this_philosipher]);
+  if(this_philosipher != 4){
+  	pthread_mutex_unlock(&fork_id[this_philosipher+1]);
+  } else {
+  	pthread_mutex_unlock(&fork_id[0]);
+  }
   return;
 }
 //set state to eating 
