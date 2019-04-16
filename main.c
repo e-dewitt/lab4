aa49ee9ac23db4fecf9aaadb3330102df42cdc2b
Ellen DeWitt <dewitt.156@osu.edu>
2019-04-15 22:52:59 -0400

    loops philosopher threads though does not include semaphores yet

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include<pthread.h> 
 #include<semaphore.h> 
 #include<stdio.h> 
+#include<time.h>
+#include<stdlib.h> 
 
 #define NUM_THREADS 5
 
@@ -11,57 +13,72 @@ int state[3];
 int philosophers[5] = {0, 1, 2, 3, 4};
 int forks[5] = {0, 1, 2, 3, 4};
 
+int max_time = 10;
+int min_time = 1;
 
-sem_t mutex;
-sem_t semaphore;
+//sem_t mutex;
+//sem_t semaphore[5];
+
   
 //Function Prototypes
-void philosopher();
-void think();
-void get_forks();
-void put_forks();
-void eat();
+void *philosopher(void *philosopher_thread);
+void think(int this_philosipher);
+void get_forks(int this_philosipher);
+void put_forks(int this_philosipher);
+void eat(int this_philosipher);
 
 int main() {
-  //create pthreads
-  pthread_t thread_id[NUM_THREADS];
+	  //create pthreads
+	  pthread_t thread_id[NUM_THREADS];
 
-  //create count variable i
-  int i;
+	  //create count variable i
+	  int i;
+	  
+	  //initialize semaphore
+	  //sem_init() returns 0 on success; on error, -1 is returned, and errno is set to indicate the error
+	  //sem_init(&mutex, 0, 1);
+
+	  //initialize a semaphore for each philosipher
+	  for(i = 0; i < NUM_THREADS; i++) {
+	    int j = 0;//sem_init(&semaphore[i], 0, 0);
+	  	if (j != 0) {
+	  		printf("\nError initializing semaphores");
+	  		return 0;
+	  	}
+	  }
+
+	  //create pthreads
+	  for(i = 0; i < NUM_THREADS; i++) {
+	    pthread_create(&thread_id[i], NULL, philosopher, &philosophers[i]);
+	  }
+
+	  for(i = 0; i < NUM_THREADS; i++) {
+	    pthread_join(thread_id[i], NULL);
+	}
   
-  //initialize semaphore
-  //sem_init() returns 0 on success; on error, -1 is returned, and errno is set to indicate the error
-  sem_init(&mutex, 0, 1);
-
-  //initialize a semaphore for each philosipher
-  for(i = 0; i < NUM_THREADS; i++) {
-    sem_init(&semaphore[i], 0, 0);
-  }
-
-  //create pthreads
-  for(i = 0; i < NUM_THREADS; i++) {
-    pthread_create(&thread_id[i], NULL, philosopher, &philosophers[i])
-  }
-  
-return 0;
+	return 0;
 }
 
 //set state of philosopher 
-void philosopher(){
- while (true) {
-    think();
-    get_forks();
-    eat();
-    put_forks();
+void *philosopher(void *philosopher_thread) {
+ int k = 1;
+ int id = *(int *)(philosopher_thread);
+ while (k) {
+    think(id);
+    get_forks(id);
+    eat(id);
+    put_forks(id);
   }
 }
 void think(int this_philosipher){
-  printf("\nPhilosopher %d is thinking", this_philosipher);
+  int time = rand() % (max_time - min_time + 1) + min_time;
+  printf("\nPhilosopher %d is thinking for %d seconds", this_philosipher, time);
   return;
 }
 //pick up chopsticks/fork
 void get_forks(int this_philosipher){
   printf("\nPhilosopher %d is getting forks", this_philosipher);
+  
   return;
 }
 //put down chopsticks/fork 
@@ -71,6 +88,7 @@ void put_forks(int this_philosipher){
 }
 //set state to eating 
 void eat(int this_philosipher){
-  printf("\nPhilosopher %d is eating!", this_philosipher);
+  int time = rand() % (max_time - min_time + 1) + min_time;
+  printf("\nPhilosopher %d is eating for %d seconds!", this_philosipher, time);
   return;
 }
